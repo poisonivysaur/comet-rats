ebf2926bfe27fff8054d7c7bdf1e6f35678d5575
Ivy <ivana_lim@dlsu.edu.ph>
2017-06-23 20:40:09 +0800

    added home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'', include('rats.urls')),
 ]
--- rats/models.py
+++ rats/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    dateCreated = models.DateTimeField(default=timezone.now)
+    dateDue = models.DateTimeField()
+    team = models.ForeignKey('Team')
+    status = models.CharField(max_length=20)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    members = []
+    roles = []
+    projects = []
+'''
+    def assignProj(self):
+        asfda
+
+    def addMember(self):
+        asdfasd
+
+    def removeMember(self):
+
+    
+    def assignRoles(self):
+
+
+class User
+'''
