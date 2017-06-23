d95e3e2294ec7010bfdaf8b8dfc781f122b13e59
Ivy <ivana_lim@dlsu.edu.ph>
2017-06-23 21:08:38 +0800

    added base html

--- rats/views.py
+++ rats/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
